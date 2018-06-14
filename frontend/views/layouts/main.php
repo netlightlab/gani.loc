3ac84c6b91b8c2f578d92c4f41f73a8f59c9b810
igor <bykov.devil@gmail.com>
2018-06-14 17:41:50 +0600

    metrika

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -148,6 +148,51 @@ $context = $this->context;
         </div>
     </div>
 </footer>
+
+
+<script type="text/javascript">
+    var yaParams = {};
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://ip.up66.ru/', true);
+    xhr.onload = function() {
+
+        yaParams.ip = this.responseText;
+    }
+    xhr.send();
+</script>
+
+
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+    (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter48947582 = new Ya.Metrika({
+                    id:48947582,
+                    params:window.yaParams,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true
+                });
+            } catch(e) { }
+        });
+
+        var n = d.getElementsByTagName("script")[0],
+            s = d.createElement("script"),
+            f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/48947582" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
 <?php $this->endBody() ?>
 </body>
 </html>
